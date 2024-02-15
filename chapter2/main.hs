80e049c0562d1d310a3a1af481dcae2e22d06ef0
Koukyosyumei <koukyosyumei@hotmail.com>
2024-02-15 14:51:48 +0900

    update

--- chapter2/main.hs
+++ chapter2/main.hs
@@ -1,6 +1,7 @@
 import           BaseLang
+import           AbstLang
 
--- Define your function definitions
+-- Define the function definitions
 funDefs :: [FunDef]
 funDefs =
   [ FunDef "add" ["x", "y"] (Add (Var "x") (Var "y")),
@@ -8,11 +9,13 @@ funDefs =
     FunDef "multiply" ["x", "y"] (Mul (Var "x") (Var "y")),
     FunDef "fac" ["n"] (If (Eq (Var "n") (Const 0))
                              (Const 1)
-                             (Mul (Var "n") (Call "fac" [(Sub (Var "n") (Const 1))])))
+                             (Mul (Var "n") (Call "fac" [(Sub (Var "n") (Const 1))]))),
+    FunDef "g" ["x", "y"] (If (Eq (Var "y") (Const 0)) 
+                                (Var "x")
+                                (Call "g" [Add (Var "x") (Const 1), Sub (Var "y") (Const 1)]))
   ]
 
-funPhi :: Phi
-funPhi = evalProgram funDefs
+-- Define the expressions
 
 exp1 :: Exp
 exp1 = Add (Const 3) (Call "add" [Const 4, Const 5])
@@ -23,7 +26,19 @@ exp2 = Call "fac" [Const 5]
 exp3 :: Exp
 exp3 = Call "fac" [Var "x"]
 
--- Evaluate the example expression using finalPhi
+exp4 :: Exp
+exp4 = Call "g" [Const 1, Const 0]
+
+-- Evaluate the function definitions
+
+funPhi :: Phi
+funPhi = evalProgram funDefs
+
+funAPhi :: APhi
+funAPhi = evalAProgram funDefs
+
+-- Evaluate the expressions
+
 result1 :: D
 result1 = evalExp exp1 funPhi []
 
@@ -33,8 +48,16 @@ result2 = evalExp exp2 funPhi []
 result3 :: D
 result3 = evalExp exp3 funPhi [("x", Just 4)]
 
+result4 :: D
+result4 = evalExp exp4 funPhi []
+
+result5 :: ATwo
+result5 = evalAExp exp4 funAPhi []
+
 main :: IO ()
 main = do
     putStrLn $ "Result1: " ++ show result1
     putStrLn $ "Result2: " ++ show result2
     putStrLn $ "Result3: " ++ show result3
+    putStrLn $ "Result4: " ++ show result4
+    putStrLn $ "Result5: " ++ show result5
