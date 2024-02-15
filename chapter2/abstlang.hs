161361987ffe1aa2a7cab116f751e661e0b00138
Koukyosyumei <koukyosyumei@hotmail.com>
2024-02-15 14:24:19 +0900

    update

--- chapter2/abstlang.hs
+++ chapter2/abstlang.hs
@@ -20,11 +20,27 @@ _ ∨ _   = Zero
 
 (<=) :: ATwo -> ATwo -> Bool
 Zero <= _ = True
-_ <= One = True
-_ <= _ = False
+_ <= One  = True
+_ <= _    = False
 
 type AEnv = [(String, ATwo)]
 type APhi = [(String, [ATwo] -> ATwo)]
+
+evalAProgram :: [FunDef] -> APhi
+evalAProgram funcs = fix (\phi -> applyAFunDefs funcs phi)
+    where
+        fix :: (a -> a) -> a
+        fix f = f (fix f)
+
+applyAFunDefs :: [FunDef] -> APhi -> APhi
+applyAFunDefs [] phi = phi
+applyAFunDefs ((FunDef s args exp):rest) phi =
+    applyAFunDefs rest ((s, \params -> evalAExp exp (updateAFunDefs (FunDef s args exp) phi) (zip args params)) : phi)
+
+updateAFunDefs :: FunDef -> APhi -> APhi
+updateAFunDefs (FunDef s args exp) phi =
+    let innerphi = [(s, \params -> evalAExp exp (updateAFunDefs (FunDef s args exp) phi) (zip args params))]
+    in innerphi ++ phi
 
 evalAExp :: Exp -> APhi -> AEnv -> ATwo
 evalAExp (Const x) _ _ = One
@@ -42,9 +58,9 @@ evalAExp (Eq x y)  phi env = deq (evalAExp x phi env) (evalAExp y phi env)
 evalAExp (GEq x y) phi env = dgeq (evalAExp x phi env) (evalAExp y phi env)
     where
         dgeq :: ATwo -> ATwo -> ATwo
-        dgeq One _ = One
+        dgeq One _    = One
         dgeq Zero One = One
-        dgeq _ _ = Zero
+        dgeq _ _      = Zero
 evalAExp (If cond thenBranch elseBranch) phi env = c ∧ (t ∨ e)
     where
         c = evalAExp cond phi env
