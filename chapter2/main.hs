529730f4046a4949116c6d6f1bd6bf1f3902945a
Koukyosyumei <koukyosyumei@hotmail.com>
2024-02-15 13:47:22 +0900

    update

--- chapter2/main.hs
+++ chapter2/main.hs
@@ -8,8 +8,28 @@ fac = FunDef "fac" ["n"] (If (Eq (Var "n") (Const 0))
 phi :: Phi
 phi = updateFunDefs fac []
 
+-- Define your function definitions
+funcDefs :: [FunDef]
+funcDefs =
+  [ FunDef "add" ["x", "y"] (Add (Var "x") (Var "y")),
+    FunDef "subtract" ["x", "y"] (Sub (Var "x") (Var "y")),
+    FunDef "multiply" ["x", "y"] (Mul (Var "x") (Var "y"))
+  ]
+
+-- Use progSemantics to get the final function environment
+finalPhi :: Phi
+finalPhi = evalProgram funcDefs
+
+exampleExp :: Exp
+exampleExp = Add (Const 3) (Call "add" [Const 4, Const 5])
+
+-- Evaluate the example expression using finalPhi
+result :: D
+result = evalExp exampleExp finalPhi []
+
 main :: IO ()
 main = do
+    putStrLn $ show result
     putStrLn $ show fac
     case phi of
         (k, f):_ -> putStrLn $ show (f [Just 4])
