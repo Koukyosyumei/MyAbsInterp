a57b098848fce30212ee71a266e9d78cce2b5bc2
Koukyosyumei <koukyosyumei@hotmail.com>
2024-02-15 14:33:50 +0900

    update

--- chapter2/main.hs
+++ chapter2/main.hs
@@ -1,37 +1,40 @@
 import           BaseLang
 
-fac :: FunDef
-fac = FunDef "fac" ["n"] (If (Eq (Var "n") (Const 0))
-                             (Const 1)
-                             (Mul (Var "n") (Call "fac" [(Sub (Var "n") (Const 1))])))
-
-phi :: Phi
-phi = updateFunDefs fac []
-
 -- Define your function definitions
-funcDefs :: [FunDef]
-funcDefs =
+funDefs :: [FunDef]
+funDefs =
   [ FunDef "add" ["x", "y"] (Add (Var "x") (Var "y")),
     FunDef "subtract" ["x", "y"] (Sub (Var "x") (Var "y")),
-    FunDef "multiply" ["x", "y"] (Mul (Var "x") (Var "y"))
+    FunDef "multiply" ["x", "y"] (Mul (Var "x") (Var "y")),
+    FunDef "fac" ["n"] (If (Eq (Var "n") (Const 0))
+                             (Const 1)
+                             (Mul (Var "n") (Call "fac" [(Sub (Var "n") (Const 1))])))
   ]
 
--- Use progSemantics to get the final function environment
-finalPhi :: Phi
-finalPhi = evalProgram funcDefs
+funPhi :: Phi
+funPhi = evalProgram funDefs
 
-exampleExp :: Exp
-exampleExp = Add (Const 3) (Call "add" [Const 4, Const 5])
+exp1 :: Exp
+exp1 = Add (Const 3) (Call "add" [Const 4, Const 5])
+
+exp2 :: Exp
+exp2 = Call "fac" [Const 5]
+
+exp3 :: Exp
+exp3 = Call "fac" [Var "x"]
 
 -- Evaluate the example expression using finalPhi
-result :: D
-result = evalExp exampleExp finalPhi []
+result1 :: D
+result1 = evalExp exp1 funPhi []
+
+result2 :: D
+result2 = evalExp exp2 funPhi []
+
+result3 :: D
+result3 = evalExp exp3 funPhi [("x", Just 4)]
 
 main :: IO ()
 main = do
-    putStrLn $ show result
-    putStrLn $ show fac
-    case phi of
-        (k, f):_ -> putStrLn $ show (f [Just 4])
-    let result = evalExp (Call "fac" [Const 5] ) phi []
-    putStrLn $ "Result: " ++ show result
+    putStrLn $ "Result1: " ++ show result1
+    putStrLn $ "Result2: " ++ show result2
+    putStrLn $ "Result3: " ++ show result3
