67f57b79d722fb683633177b7260bfdc3826c4f6
Koukyosyumei <koukyosyumei@hotmail.com>
2024-02-17 09:33:04 +0900

    update

--- strictness/main.hs
+++ strictness/main.hs
@@ -5,6 +5,15 @@ import           FixedPointIteration
 import qualified Data.HashMap.Strict as HashMap
 import           Data.Hashable       (Hashable)
 
+-- Define the demo functions for fixed point iteration
+g' :: [Int] -> HashMap.HashMap [Int] Int -> Int
+g' (x:y:rest) table = min y (max x (HashMap.lookupDefault 0 [x, y] table))
+
+f' :: [Int] -> HashMap.HashMap [Int] Int -> Int
+f' (x:y:z:rest) table =
+  let t = HashMap.lookupDefault 0 [y, 1, 1] table
+  in max (min y z) (HashMap.lookupDefault 0 [z, x, t] table)
+
 -- Define the function expressions
 add :: Exp
 add = Add (Var "x") (Var "y")
@@ -27,14 +36,13 @@ funDefs =
     FunDef "g'" ["x", "y"] (transformExpWithMemo g)
   ]
 
--- Define the demo functions for fixed point iteration
-g' :: [Int] -> HashMap.HashMap [Int] Int -> Int
-g' (x:y:rest) table = min y (max x (HashMap.lookupDefault 0 [x, y] table))
+-- Evaluate the function definitions
 
-f' :: [Int] -> HashMap.HashMap [Int] Int -> Int
-f' (x:y:z:rest) table =
-  let t = HashMap.lookupDefault 0 [y, 1, 1] table
-  in max (min y z) (HashMap.lookupDefault 0 [z, x, t] table)
+funPhi :: Phi
+funPhi = evalProgram funDefs
+
+funAPhi :: APhi
+funAPhi = evalAProgram funDefs
 
 -- Define the expressions
 
@@ -47,25 +55,19 @@ callFacConst = Call "fac" [Const 5]
 callFacVar :: Exp
 callFacVar = Call "fac" [Var "x"]
 
+callG :: Exp
+callG = Call "g" [Var "x", Var "y"]
+
 fpicallG' :: Exp
 fpicallG' = FPICall "g'" [Var "x", Var "y"]
-
--- Evaluate the function definitions
-
-funPhi :: Phi
-funPhi = evalProgram funDefs
-
-funAPhi :: APhi
-funAPhi = evalAProgram funDefs
 
 -- Evaluate the expressions
 
 main :: IO ()
 main = do
-    putStrLn $ "Demonstration of Fixed Point Iteration"
     let tableG = HashMap.fromList [([0, 0], 0), ([1, 0], 0), ([0, 1], 0), ([1, 1], 0)]
         resultG = fixedpointIteration g' tableG 0
-    putStrLn $ "Iterations for g: " ++ show (fst resultG)
+    putStrLn $ "Fixed point iterations for g: " ++ show (fst resultG)
     print (snd resultG)
 
     let tableF =
@@ -80,37 +82,23 @@ main = do
             ([1, 1, 1], 0)
             ]
         resultF = fixedpointIteration f' tableF 0
-    putStrLn $ "Iterations for f: " ++ show (fst resultF)
+    putStrLn $ "Fixed point iterations for f: " ++ show (fst resultF)
     print (snd resultF)
     putStrLn "-----------------------"
 
-    let
-
-result1 :: D
-result1 = evalExp exp1 funPhi []
-
-result2 :: D
-result2 = evalExp exp2 funPhi []
-
-result3 :: D
-result3 = evalExp exp3 funPhi [("x", Just 4)]
-
-result4 :: D
-result4 = evalExp exp4 funPhi [("x", Just 4), ("y", Just 3)]
-
-result5 :: ATwo
-result5 = evalAExp exp4 funAPhi [("x", One), ("y", Zero)]
-
-result6 :: ATwo
-result6 = evalAExp exp4 funAPhi [("x", Zero), ("y", One)]
-
-result7 :: ATwo
-result7 = evalAExp exp4 funAPhi [("x", One), ("y", One)]
-
-    putStrLn $ "Result1: " ++ show result1
-    putStrLn $ "Result2: " ++ show result2
-    putStrLn $ "Result3: " ++ show result3
-    -- putStrLn $ "Result4: " ++ show result4
-    putStrLn $ "Result5: " ++ show result5
-    putStrLn $ "Result6: " ++ show result6
-    putStrLn $ "Result7: " ++ show result7
+    let resultCallAdd = evalExp callAdd funPhi []
+        resultCallFacConst = evalExp callFacConst funPhi []
+        resultCallFacVar = evalExp callFacVar funPhi [("x", Just 4)]
+        resultCallG = evalExp callG funPhi [("x", Just 4), ("y", Just 3)]
+        resultFPICallGZeroZero = evalAExp fpicallG' funAPhi [("x", Zero), ("y", Zero)]
+        resultFPICallGOneZero = evalAExp fpicallG' funAPhi [("x", One), ("y", Zero)]
+        resultFPICallGZeroOne = evalAExp fpicallG' funAPhi [("x", Zero), ("y", One)]
+        resultFPICallGOneOne = evalAExp fpicallG' funAPhi [("x", One), ("y", One)]
+    putStrLn $ "add(4, 5) = " ++ show (resultCallAdd)
+    putStrLn $ "fac(5) = " ++ show (resultCallFacConst)
+    putStrLn $ "x = 4; fac(x) = " ++ show (resultCallFacVar)
+    putStrLn $ "x = 4; y = 3; g(4, 3) = " ++ show (resultCallG)
+    putStrLn $ "x = Zero; y = Zero; g#(x, y) = " ++ show (resultFPICallGZeroZero)
+    putStrLn $ "x = One; y = Zero; g#(x, y) = " ++ show (resultFPICallGOneZero)
+    putStrLn $ "x = Zero; y = One; g#(x, y) = " ++ show (resultFPICallGZeroOne)
+    putStrLn $ "x = One; y = One; g#(x, y) = " ++ show (resultFPICallGOneOne)
