bf82c68ff4aa89abef5f38ff56a9b3d20617b393
Koukyosyumei <koukyosyumei@hotmail.com>
2024-02-17 09:39:37 +0900

    update

--- strictness/baselang.hs
+++ strictness/baselang.hs
@@ -1,11 +1,18 @@
 module BaseLang where
 
+-- | Type synonym for representing values.
 type V = Int
+
+-- | Type synonym for representing computations that may fail.
 type D = Maybe Int
 
+-- | Type alias for environment bindings.
 type Env = [(String, D)]
+
+-- | Type alias for function signature mappings.
 type Phi = [(String, [D] -> D)]
 
+-- | Represents expressions in the language.
 data Exp = Const Int
     | Var String
     | Add Exp Exp
@@ -19,25 +26,38 @@ data Exp = Const Int
     | FPICall String [Exp]
     | StrictCall String [Exp] deriving(Show)
 
+-- | Represents function definitions.
 data FunDef = FunDef String [String] Exp deriving(Show)
 
-evalProgram :: [FunDef] -> Phi
+-- | Evaluates a program represented by a list of function definitions.
+evalProgram :: [FunDef]    -- ^ List of function definitions.
+            -> Phi         -- ^ Function signature mappings.
 evalProgram funcs = fix (\phi -> applyFunDefs funcs phi)
     where
         fix :: (a -> a) -> a
         fix f = f (fix f)
 
-applyFunDefs :: [FunDef] -> Phi -> Phi
+-- | Applies function definitions to function signature mappings.
+applyFunDefs :: [FunDef]  -- ^ List of function definitions.
+             -> Phi       -- ^ Function signature mappings.
+             -> Phi       -- ^ Updated function signature mappings.
 applyFunDefs [] phi = phi
 applyFunDefs ((FunDef s args exp):rest) phi =
     applyFunDefs rest ((s, \params -> evalExp exp (updateFunDefs (FunDef s args exp) phi) (zip args params)) : phi)
 
-updateFunDefs :: FunDef -> Phi -> Phi
+-- | Updates function signature mappings with a new function definition.
+updateFunDefs :: FunDef  -- ^ Function definition.
+              -> Phi     -- ^ Function signature mappings.
+              -> Phi     -- ^ Updated function signature mappings.
 updateFunDefs (FunDef s args exp) phi =
     let innerphi = [(s, \params -> evalExp exp (updateFunDefs (FunDef s args exp) phi) (zip args params))]
     in innerphi ++ phi
 
-evalExp :: Exp -> Phi -> Env -> D
+-- | Evaluates an expression in the given environment.
+evalExp :: Exp   -- ^ Expression to be evaluated.
+        -> Phi   -- ^ Function signature mappings.
+        -> Env   -- ^ Environment containing variable bindings.
+        -> D     -- ^ Result of the evaluation.
 evalExp (Const x) _ _ = Just x
 evalExp (Var key) _ env =
     case lookup key env of
@@ -73,7 +93,9 @@ evalExp (StrictCall fname args) phi env =
         strict f args =
             if noneIsNothing args then f args else Nothing
 
-noneIsNothing :: [D] -> Bool
+-- | Checks if all elements in the list are not 'Nothing'.
+noneIsNothing :: [D]  -- ^ List of 'D' values.
+              -> Bool -- ^ 'True' if all elements are 'Just', 'False' otherwise.
 noneIsNothing = all isJust
   where
     isJust :: Maybe a -> Bool
